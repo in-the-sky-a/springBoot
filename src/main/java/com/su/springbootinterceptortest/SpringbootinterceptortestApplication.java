41d3f5ed603cc290df57fc0d7447c69686efffc1
suqianqian <suqianqian@wecash.net>
2019-08-08 11:35:41 +0800

    first commit

---
+++ src/main/java/com/su/springbootinterceptortest/SpringbootinterceptortestApplication.java
@@ -0,0 +1,13 @@
+package com.su.springbootinterceptortest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootinterceptortestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootinterceptortestApplication.class, args);
+    }
+
+}
